3e8c5c4ced22d3acffbd6f0eebd031505e598991
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-03-26 16:35:22 -0500

    practicing with maven

--- src/main/java/MavenTest.java
+++ src/main/java/MavenTest.java
@@ -1,13 +1,50 @@
 import org.apache.commons.lang3.StringUtils;
 
-public class MavenTest {
+import java.util.Scanner;
 
+public class MavenTest {
 
     public static void main(String[] args) {
 
-        System.out.println(StringUtils.reverse("Hello my name is Ben"));
-        System.out.println(StringUtils.isNumeric("123"));
-        System.out.println(StringUtils.swapCase("Ben Rasmussen"));
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Something: ");
+
+        String userInput = sc.nextLine();
+
+        System.out.println("You Entered: " + "\"" +  userInput + "\"");
+
+        String message;
+        if (stringIsANumber(userInput)) {
+            message = " is a number";
+        } else {
+            message = " is not a number";
+        }
+
+        System.out.println(userInput + message);
+        System.out.println("Flipped Case: " + flipTheStringCase(userInput));
+        System.out.println("Reversed: " + reverseTheString(userInput));
+
+        // expected output
+
+//        Enter Something: Life, The Universe, and Everything
+//        You Entered: "Life, The Universe, and Everything"
+//        "Life, The Universe, and Everything" is not a number
+//        Flipped Case: lIFE, tHE uNIVERSE, AND eVERYTHING
+//        Reversed: gnihtyrevE dna ,esrevinU eht ,efiL
 
     }
+
+    public static boolean stringIsANumber(String input) {
+        return StringUtils.isNumeric(input);
+    }
+
+    public static String flipTheStringCase(String input) {
+        return StringUtils.swapCase(input);
+    }
+
+    public static String reverseTheString(String input) {
+        return StringUtils.reverse(input);
+    }
+
 }
