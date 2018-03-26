3ef4c96b0a5b18188879c058960421c751558a4e
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-03-26 11:05:14 -0500

    working on maven exercise

---
+++ src/main/java/MavenTest.java
@@ -0,0 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class MavenTest {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(StringUtils.reverse("Hello my name is Ben"));
+        System.out.println(StringUtils.isNumeric("123"));
+        System.out.println(StringUtils.swapCase("Ben Rasmussen"));
+
+    }
+}
